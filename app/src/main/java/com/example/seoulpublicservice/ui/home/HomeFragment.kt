04b7245d6f1719a569ad771f65e5887db4258da6
Hong <151532167+UChangh@users.noreply.github.com>
2024-02-21 16:48:36 +0900

    git init YC

--- app/src/main/java/com/example/seoulpublicservice/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/seoulpublicservice/ui/home/HomeFragment.kt
@@ -31,6 +31,7 @@ class HomeFragment : Fragment() {
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
+            textView.text = "YCccc"
         }
         return root
     }
