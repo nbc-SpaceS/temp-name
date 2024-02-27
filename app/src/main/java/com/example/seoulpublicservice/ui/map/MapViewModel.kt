42d694800790601e23ef4ba1526519166add1751
phs8505 <phs8505@gmail.com>
2024-02-27 17:45:27 +0900

    feat : 필터 적용하기 버튼을 눌렀을 때 지도 프래그먼트에서 Room DB에 쿼리 보내는 기능
    Room 쿼리에 이상한 오류가 있어서 아직 결과를 보지는 못 했음
    
    필터 적용하기를 누르면 람다식으로 만들어놓은 함수가 지도 프래그먼트에서 실행되고 거기 안에는 SharedPrefernce에 접근해 데이터를 가져오고 가져온 데이터를 가지고 쿼리를 날리는 함수가 추가되어있음
    
    데이터를 받아오는 건 코루틴을 사용하여 했고 IO 디스패처로 진행
    
    setValue로 LiveData에 값을 넣으려니까 에러가 나서 백그라운드에서 값을 넣는 postValue로 변경

--- app/src/main/java/com/example/seoulpublicservice/ui/map/MapViewModel.kt
+++ app/src/main/java/com/example/seoulpublicservice/ui/map/MapViewModel.kt
@@ -3,11 +3,57 @@ package com.example.seoulpublicservice.ui.map
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.seoulpublicservice.SeoulPublicServiceApplication
+import com.example.seoulpublicservice.databases.ReservationEntity
+import com.example.seoulpublicservice.databases.ReservationRepository
+import com.example.seoulpublicservice.pref.FilterPrefRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class MapViewModel : ViewModel() {
+class MapViewModel(
+    private val filterPrefRepository: FilterPrefRepository,
+    private val reservationRepository: ReservationRepository
+) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    private val _loadedFilterOptions: MutableLiveData<List<List<String>>> = MutableLiveData()
+    val loadedFilterOptions: LiveData<List<List<String>>> get() = _loadedFilterOptions
+
+    private val _hasFilter: MutableLiveData<Boolean> = MutableLiveData()
+    val hasFilter: LiveData<Boolean> get() = _hasFilter
+
+    private val _filteringData: MutableLiveData<List<ReservationEntity>> = MutableLiveData()
+    val filteringData: LiveData<List<ReservationEntity>> get() = _filteringData
+
+    fun load() {
+        val loadedData = filterPrefRepository.load()
+
+        _loadedFilterOptions.value = loadedData
+
+        _hasFilter.value = loadedData.any { it.isNotEmpty() }
+
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                _filteringData.postValue(reservationRepository.getReservationsWithSmallTypes(loadedData[0]))
+            }
+        }
     }
-    val text: LiveData<String> = _text
+
+    companion object {
+        /** 뷰모델팩토리에서 의존성주입을 해준다 */
+        val factory = viewModelFactory {
+            initializer {
+                val application = (this[APPLICATION_KEY] as SeoulPublicServiceApplication)
+                val container = application.container
+                MapViewModel(
+                    filterPrefRepository = container.filterPrefRepository,
+                    reservationRepository = container.reservationRepository
+                )
+            }
+        }
+    }
 }
