87e188c232f64b956197f95f668a1386a399f708
jericho31 <a@a.a>
2024-02-27 19:43:24 +0900

    feat: MyPage 멀티뷰타입 구현
    sealed interface랑 CommonViewHolder도 어댑터에 집어넣음

--- app/src/main/java/com/example/seoulpublicservice/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/example/seoulpublicservice/ui/mypage/MyPageFragment.kt
@@ -1,12 +1,13 @@
 package com.example.seoulpublicservice.ui.mypage
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.seoulpublicservice.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.seoulpublicservice.databinding.FragmentMyPageBinding
+import com.example.seoulpublicservice.seoul.Row
 
 class MyPageFragment : Fragment() {
 
@@ -14,19 +15,72 @@ class MyPageFragment : Fragment() {
         fun newInstance() = MyPageFragment()
     }
 
-    private lateinit var viewModel: MyPageViewModel
+    private var _binding: FragmentMyPageBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: MyPageViewModel by viewModels()
+
+    private val myPageSavedAdapter = MyPageSavedAdapter().apply {
+        var a = 0
+        submitList(
+            listOf(
+                Row.new(svcnm = "첫 번째 제목~~", areanm = "가가구", svcstatnm = "접수${++a}"),
+                Row.new(svcnm = "두 번째 제목~~", areanm = "나나구", svcstatnm = "접수${++a}"),
+                Row.new(svcnm = "세 번째 제목~~", areanm = "다다구", svcstatnm = "접수${++a}"),
+                Row.new(svcnm = "첫 번째 제목~~", areanm = "가가구", svcstatnm = "접수${++a}"),
+                Row.new(svcnm = "두 번째 제목~~", areanm = "나나구", svcstatnm = "접수${++a}"),
+                Row.new(svcnm = "세 번째 제목~~", areanm = "다다구", svcstatnm = "접수${++a}"),
+                Row.new(svcnm = "첫 번째 제목~~", areanm = "가가구", svcstatnm = "접수${++a}"),
+                Row.new(svcnm = "두 번째 제목~~", areanm = "나나구", svcstatnm = "접수${++a}"),
+                Row.new(svcnm = "세 번째 제목~~", areanm = "다다구", svcstatnm = "접수${++a}"),
+            )
+        )
+    }
+    private val myPageAdapter = MyPageAdapter().apply {
+        var a = 0
+        submitList(
+            listOf(
+                MyPageAdapter.MultiView.Profile,
+                MyPageAdapter.MultiView.Saved(myPageSavedAdapter),
+                MyPageAdapter.MultiView.ReviewedHeader
+            )
+                    + listOf(
+                MyPageAdapter.MultiView.Reviewed(Row.new(svcnm = "${++a} 번째 제목~~", areanm = "가가구")),
+                MyPageAdapter.MultiView.Reviewed(Row.new(svcnm = "${++a} 번째 제목~~", areanm = "나나구")),
+                MyPageAdapter.MultiView.Reviewed(Row.new(svcnm = "${++a} 번째 제목~~", areanm = "다다구")),
+                MyPageAdapter.MultiView.Reviewed(Row.new(svcnm = "${++a} 번째 제목~~", areanm = "가가구")),
+                MyPageAdapter.MultiView.Reviewed(Row.new(svcnm = "${++a} 번째 제목~~", areanm = "나나구")),
+                MyPageAdapter.MultiView.Reviewed(Row.new(svcnm = "${++a} 번째 제목~~", areanm = "다다구")),
+                MyPageAdapter.MultiView.Reviewed(Row.new(svcnm = "${++a} 번째 제목~~", areanm = "가가구")),
+                MyPageAdapter.MultiView.Reviewed(Row.new(svcnm = "${++a} 번째 제목~~", areanm = "나나구")),
+                MyPageAdapter.MultiView.Reviewed(Row.new(svcnm = "${++a} 번째 제목~~", areanm = "다다구")),
+            )
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_my_page, container, false)
+    ): View {
+        _binding = FragmentMyPageBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MyPageViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+        initViewModel()
+    }
+
+    private fun initView() = binding.let { b ->
+        b.rvMyPage.adapter = myPageAdapter
+    }
+
+    private fun initViewModel() {
     }
 
 }
