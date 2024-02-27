b1becd5f4eb21f73304acf8b611c1754e36086f7
ChooJeongHo <kd64choo@gmail.com>
2024-02-27 16:35:11 +0900

    Feat : MainFragment Layout 제작, ViewPager + TabLayout 으로 각 탭 표현, 각 탭에 GridView RecyclerView로 항목 정의, 아이템 클릭시 배경이랑 아이콘 색 바뀜

---
+++ app/src/main/java/com/example/seoulpublicservice/ui/home/MedicalFragment.kt
@@ -0,0 +1,38 @@
+package com.example.seoulpublicservice.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.seoulpublicservice.R
+import com.example.seoulpublicservice.adapter.ItemAdapter
+import com.example.seoulpublicservice.data.Item
+import com.example.seoulpublicservice.databinding.FragmentFacilityBinding
+import com.example.seoulpublicservice.databinding.FragmentMedicalBinding
+
+class MedicalFragment : Fragment() {
+    private var _binding: FragmentMedicalBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentMedicalBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val items = listOf(
+            Item(R.drawable.ic_hospital, "병원"),
+            Item(R.drawable.ic_bus, "장애인버스"),
+            Item(R.drawable.ic_childhospital, "어린이병원"),
+        )
+
+        val adapter = ItemAdapter(items)
+        binding.rvMedical.adapter = adapter
+        binding.rvMedical.layoutManager = GridLayoutManager(requireContext(), 4)
+    }
+}
