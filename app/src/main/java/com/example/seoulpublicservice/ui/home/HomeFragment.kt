fd2eab0999895457bb817cefa128f4d98f26a28e
ChooJeongHo <kd64choo@gmail.com>
2024-02-28 19:35:38 +0900

    Feat : 스피너 추가, 스타일 설정

--- app/src/main/java/com/example/seoulpublicservice/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/seoulpublicservice/ui/home/HomeFragment.kt
@@ -1,18 +1,24 @@
 package com.example.seoulpublicservice.ui.home
 
+import android.content.Context
 import android.content.Intent
 import androidx.navigation.fragment.findNavController
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.seoulpublicservice.InterestRegionSelectActivity
 import com.example.seoulpublicservice.R
+import com.example.seoulpublicservice.SeoulPublicServiceApplication
+import com.example.seoulpublicservice.adapter.SpinnerAdapter
 import com.example.seoulpublicservice.databinding.FragmentHomeBinding
+import com.example.seoulpublicservice.pref.RegionPrefRepository
 import com.example.seoulpublicservice.ui.notifications.NotificationsFragment
 import com.google.android.material.tabs.TabLayoutMediator
 
@@ -20,6 +26,15 @@ class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private val regionPrefRepository: RegionPrefRepository by lazy {
+        (requireActivity().application as SeoulPublicServiceApplication).container.regionPrefRepository
+    }
+    private var fragmentContext: Context? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        fragmentContext = context
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
@@ -31,6 +46,7 @@ class HomeFragment : Fragment() {
 
         val viewPager = binding.viewPager
         val tabLayout = binding.tabLayout
+
 
         // 지역 선택 화면으로 이동
         binding.tvSelectArea.setOnClickListener {
@@ -67,6 +83,53 @@ class HomeFragment : Fragment() {
                 4 -> tab.text = "진료"
             }
         }.attach()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val selectedRegions = regionPrefRepository.load().toMutableList()
+        if (selectedRegions.isNotEmpty()) {
+
+            // 스피너에 관심지역 설정 항목 추가
+            selectedRegions.add("관심지역 설정")
+//            fragmentContext?.let {
+//                val adapter = SpinnerAdapter(it, R.layout.item_spinner, selectedRegions)
+//                binding.spinnerSelectArea.adapter = adapter
+//                binding.spinnerSelectArea.setBackgroundResource(R.drawable.spinner_background)
+//            }
+            fragmentContext?.let {
+                val adapter = ArrayAdapter(it, android.R.layout.simple_spinner_dropdown_item, selectedRegions)
+                binding.spinnerSelectArea.adapter = adapter
+                binding.spinnerSelectArea.setBackgroundResource(R.drawable.spinner_background)
+            }
+
+
+            // Show the Spinner and hide the TextView
+            binding.spinnerSelectArea.visibility = View.VISIBLE
+            binding.tvSelectArea.visibility = View.GONE
+
+            binding.spinnerSelectArea.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                    val selectedItem = parent.getItemAtPosition(position).toString()
+
+                    // "관심지역 설정" 항목을 선택하면 관심지역 설정 페이지로 이동
+                    if (selectedItem == "관심지역 설정") {
+                        val intent = Intent(context, InterestRegionSelectActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // 아무 항목도 선택되지 않았을 때의 동작
+                }
+            }
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        fragmentContext = null
     }
 
     override fun onDestroyView() {
