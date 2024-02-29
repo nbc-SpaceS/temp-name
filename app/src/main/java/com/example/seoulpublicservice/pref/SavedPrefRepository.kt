8acd9862786ae043780555e4b991d6b99d4607c7
jericho31 <a@a.a>
2024-02-29 11:18:04 +0900

    feat: SavedPrefRepository 개별 추가

--- app/src/main/java/com/example/seoulpublicservice/pref/SavedPrefRepository.kt
+++ app/src/main/java/com/example/seoulpublicservice/pref/SavedPrefRepository.kt
@@ -7,6 +7,7 @@ import com.google.gson.Gson
 interface SavedPrefRepository {
     fun saveSvcidList(svcidList: List<String>)
     fun loadSvcidList(): List<String>
+    fun saveSvcid(svcid: String)
 }
 
 class SavedPrefRepositoryImpl(context: Context) : SavedPrefRepository {
@@ -26,6 +27,12 @@ class SavedPrefRepositoryImpl(context: Context) : SavedPrefRepository {
             .apply { Log.w("jj-SavedPrefRepositoryImpl", "loadSvcidList got null") }
         Log.d("jj-SavedPrefRepositoryImpl", "loadSvcidList json: ${json.take(255)}")
         return gson.fromJson(json, Array<String>::class.java).toList()
+    }
+
+    override fun saveSvcid(svcid: String) {
+        val list = loadSvcidList()
+        val json = gson.toJson(list + svcid)
+        pref.edit().putString(keySvcidList, json).apply()
     }
 
 }
