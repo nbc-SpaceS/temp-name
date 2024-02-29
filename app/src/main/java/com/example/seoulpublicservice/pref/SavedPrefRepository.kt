53e0629ad251d2f108f5536087508d778915404f
jericho31 <a@a.a>
2024-02-29 15:59:09 +0900

    rename: SavedPrefRepository 메서드 4개
    saveSvcidList -> addSvcidList
    loadSvcidList -> getSvcidList
    saveSvcid -> addSvcid
    save -> setSvcidList

--- app/src/main/java/com/example/seoulpublicservice/pref/SavedPrefRepository.kt
+++ app/src/main/java/com/example/seoulpublicservice/pref/SavedPrefRepository.kt
@@ -5,9 +5,9 @@ import android.util.Log
 import com.google.gson.Gson
 
 interface SavedPrefRepository {
-    fun saveSvcidList(svcidList: List<String>)
-    fun loadSvcidList(): List<String>
-    fun saveSvcid(svcid: String)
+    fun addSvcidList(svcidList: List<String>)
+    fun getSvcidList(): List<String>
+    fun addSvcid(svcid: String)
     fun clear()
     fun remove(svcid: String)
 }
@@ -19,25 +19,25 @@ class SavedPrefRepositoryImpl(context: Context) : SavedPrefRepository {
 
     private val keySvcidList = "keySvcidList"
 
-    private fun save(list: List<String>) {
+    private fun setSvcidList(list: List<String>) {
         val json = gson.toJson(list)
         pref.edit().putString(keySvcidList, json).apply()
         Log.d("jj-SavedPrefRepositoryImpl", "save json: ${json.take(255)}")
     }
 
-    override fun loadSvcidList(): List<String> {
+    override fun getSvcidList(): List<String> {
         val json = pref.getString(keySvcidList, null) ?: return emptyList<String>()
             .apply { Log.w("jj-SavedPrefRepositoryImpl", "loadSvcidList got null") }
         Log.d("jj-SavedPrefRepositoryImpl", "loadSvcidList json: ${json.take(255)}")
         return gson.fromJson(json, Array<String>::class.java).toList()
     }
 
-    override fun saveSvcidList(svcidList: List<String>) = save(loadSvcidList() + svcidList)
+    override fun addSvcidList(svcidList: List<String>) = setSvcidList(getSvcidList() + svcidList)
 
-    override fun saveSvcid(svcid: String) = save(loadSvcidList() + svcid)
+    override fun addSvcid(svcid: String) = setSvcidList(getSvcidList() + svcid)
 
     override fun clear() = pref.edit().clear().apply()
 
-    override fun remove(svcid: String) = save(loadSvcidList() - svcid)
+    override fun remove(svcid: String) = setSvcidList(getSvcidList() - svcid)
 
 }
