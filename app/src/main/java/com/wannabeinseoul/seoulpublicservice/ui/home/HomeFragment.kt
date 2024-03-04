826d2d8bf15e305cc14e49c411cc4ce33b82cd06
phs8505 <phs8505@gmail.com>
2024-03-04 22:41:52 +0900

    Layout : HomeFragment 레이아웃 수정
    1. 바텀 네비게이션 아이콘, 텍스트 수정
    
    2. 관심지역 선택 스피너 버그나는 레이아웃 수정
    
    3. 전체적인 margin 추가
    
    4. 바텀 네비게이션 구분하는 Divider 추가

--- app/src/main/java/com/wannabeinseoul/seoulpublicservice/ui/home/HomeFragment.kt
+++ app/src/main/java/com/wannabeinseoul/seoulpublicservice/ui/home/HomeFragment.kt
@@ -100,6 +100,9 @@ class HomeFragment : Fragment() {
             binding.tvHomeSelectRegion1.setTextColor(requireContext().getColor(R.color.point_color))
             binding.tvHomeSelectRegion2.setTextColor(requireContext().getColor(R.color.unable_button_text))
             binding.tvHomeSelectRegion3.setTextColor(requireContext().getColor(R.color.unable_button_text))
+            binding.clHomeRegionList.isVisible = false
+            binding.ivHomeMoreBtn.setImageResource(R.drawable.ic_more)
+            binding.viewControlSpinner.isVisible = false
         }
 
         binding.tvHomeSelectRegion2.setOnClickListener {
@@ -107,6 +110,9 @@ class HomeFragment : Fragment() {
             binding.tvHomeSelectRegion2.setTextColor(requireContext().getColor(R.color.point_color))
             binding.tvHomeSelectRegion1.setTextColor(requireContext().getColor(R.color.unable_button_text))
             binding.tvHomeSelectRegion3.setTextColor(requireContext().getColor(R.color.unable_button_text))
+            binding.clHomeRegionList.isVisible = false
+            binding.ivHomeMoreBtn.setImageResource(R.drawable.ic_more)
+            binding.viewControlSpinner.isVisible = false
         }
 
         binding.tvHomeSelectRegion3.setOnClickListener {
@@ -114,6 +120,9 @@ class HomeFragment : Fragment() {
             binding.tvHomeSelectRegion3.setTextColor(requireContext().getColor(R.color.point_color))
             binding.tvHomeSelectRegion1.setTextColor(requireContext().getColor(R.color.unable_button_text))
             binding.tvHomeSelectRegion2.setTextColor(requireContext().getColor(R.color.unable_button_text))
+            binding.clHomeRegionList.isVisible = false
+            binding.ivHomeMoreBtn.setImageResource(R.drawable.ic_more)
+            binding.viewControlSpinner.isVisible = false
         }
 
         binding.viewControlSpinner.setOnClickListener {
