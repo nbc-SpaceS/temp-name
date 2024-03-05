dc05925b0f2efd25f106b237b643b7f2d8be30da
phs8505 <phs8505@gmail.com>
2024-03-05 20:25:51 +0900

    Feat : 후기 기능 초기 버전 완료
    후기를 작성하면 UI에 바로 업데이트가 되고 후기 작성하는 페이지가 열렸을 때 이전에 작성되어있던 후기들이 UI에 노출되도록 기능 구현
    
    테이블은 서비스, 리뷰, 유저 3개 만들고 이에 해당하는 레포지토리도 각각 생성
    
    (***중요***) 앱을 한번 삭제하고 진행해야지 기능이 제대로 동작하는 걸 볼 수 있습니다.
    파이어베이스 데이터베이스에 유저정보를 저장하는 조건이 유저ID가 SharedPreference에 있냐 없냐기 때문에 삭제말고 IdPreference 내용 지우는 것도 되긴 할 거에요
    그래도 추천하는건 앱 한번 삭제하는 겁니다!

--- app/src/main/java/com/wannabeinseoul/seoulpublicservice/databases/firebase/ReviewRepository.kt
+++ app/src/main/java/com/wannabeinseoul/seoulpublicservice/databases/firebase/ReviewRepository.kt
@@ -1,46 +1,12 @@
 package com.wannabeinseoul.seoulpublicservice.databases.firebase
 
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
-
 interface ReviewRepository {
-    fun addReview(review: ReviewEntity)
-
-    fun getServiceReviews(
-        id: String,
-        listener: (List<ReviewEntity>) -> Unit
-    )
+    fun addReview(reviewId: String, reviewInfo: ReviewEntity)
 }
 
 class ReviewRepositoryImpl: ReviewRepository {
-    override fun addReview(review: ReviewEntity) {
-        FBRef.reviewRef.child(review.svcId ?: "").child(review.userId ?: "").setValue(review)
-    }
 
-    override fun getServiceReviews(
-        id: String,
-        listener: (List<ReviewEntity>) -> Unit
-    ) {
-        FBRef.reviewRef.child(id).addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                var serviceReviews = mutableListOf<ReviewEntity>()
-
-                for (data in snapshot.children) {
-                    val hashMap = data.value as HashMap<*, *>
-                    serviceReviews.add(ReviewEntity(
-                        hashMap["svcId"] as String,
-                        hashMap["userId"] as String,
-                        hashMap["content"] as String
-                    ))
-                }
-
-                listener(serviceReviews)
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
-            }
-        })
+    override fun addReview(reviewId: String, reviewInfo: ReviewEntity) {
+        FBRef.reviewRef.child(reviewId).setValue(reviewInfo)
     }
 }
