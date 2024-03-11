71d94933ba89721c1acb78ebeca0f1a4ef894bf8
phs8505 <phs8505@gmail.com>
2024-03-11 15:47:21 +0900

    Layout & Feat : 마커 색깔 설명, 상세정보창 책갈피 기능 변경
    마커 색깔에 맞는 대분류명을 알려주는 UI 추가
    총 개수를 알려주던 책갈피를 현재 데이터 순서를 알려주는 기능으로 변경

--- app/src/main/java/com/wannabeinseoul/seoulpublicservice/ui/map/MapFragment.kt
+++ app/src/main/java/com/wannabeinseoul/seoulpublicservice/ui/map/MapFragment.kt
@@ -15,6 +15,7 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.viewpager2.widget.ViewPager2
 import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraAnimation
@@ -135,7 +136,12 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     private fun initView() {
         binding.vpMapDetailInfo.adapter = adapter
-        binding.vpMapDetailInfo.registerOnPageChangeCallback(object : OnPageChangeCallback() {})
+        binding.vpMapDetailInfo.registerOnPageChangeCallback(object : OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                binding.tvMapInfoCount.text = "${position + 1}"
+            }
+        })
         binding.vpMapDetailInfo.offscreenPageLimit = 1
 
         binding.rvMapSelectedOption.adapter = rvAdapter
@@ -156,6 +162,12 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         }
 
         viewModel.initMap()
+        mainViewModel.applyFilter.observe(viewLifecycleOwner) {
+            if (it) {
+                viewModel.loadSavedOptions()
+                rvAdapter.submitList(app.container.filterPrefRepository.load().flatten())
+            }
+        }
     }
 
     private fun initViewModel() = with(viewModel) {
@@ -181,7 +193,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
         updateData.observe(viewLifecycleOwner) { list ->
             adapter.submitList(list.toList())
-            binding.tvMapInfoCount.text = list.size.toString()
+            binding.tvMapInfoCount.text = "1"
         }
 
         moveToUrl.observe(viewLifecycleOwner) {
@@ -231,13 +243,6 @@ class MapFragment : Fragment(), OnMapReadyCallback {
                         true
                     }
                 }
-            }
-        }
-
-        mainViewModel.applyFilter.observe(viewLifecycleOwner) {
-            if (it) {
-                viewModel.loadSavedOptions()
-                rvAdapter.submitList(app.container.filterPrefRepository.load().flatten())
             }
         }
     }
@@ -293,7 +298,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
                 y ?: app.lastLocation?.latitude ?: 37.5666,
                 x ?: app.lastLocation?.longitude ?: 126.9782
             ),
-            15.0
+            14.5
         ).animate(CameraAnimation.Easing, 600)
 
         naverMap.moveCamera(cameraUpdate)
