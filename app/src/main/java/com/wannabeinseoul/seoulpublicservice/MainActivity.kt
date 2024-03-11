9ab2c69df25fcee14625e59de40058a004cc2ce1
ChooJeongHo <kd64choo@gmail.com>
2024-03-11 11:52:04 +0900

    Feat : 검색창 밖 아무 곳이나 클릭했을 때, 포커스가 사라지고, 키보드가 내려가도록 구현, 일부 주석 삭제 및 import문 삭제

--- app/src/main/java/com/wannabeinseoul/seoulpublicservice/MainActivity.kt
+++ app/src/main/java/com/wannabeinseoul/seoulpublicservice/MainActivity.kt
@@ -3,16 +3,17 @@ package com.wannabeinseoul.seoulpublicservice
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
-import android.graphics.Color
+import android.graphics.Rect
 import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.wannabeinseoul.seoulpublicservice.databinding.ActivityMainBinding
@@ -95,5 +96,21 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return true
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        if (ev.action == MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(ev.rawX.toInt(), ev.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.windowToken, 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
