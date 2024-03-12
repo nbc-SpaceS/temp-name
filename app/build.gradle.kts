bf4345e0f9b0e7187ea1b29f37007e3e955acd6a
jericho31 <a@a.a>
2024-03-12 17:04:44 +0900

    chore: versionCode = 3, versionName = "1.0.2"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.wannabeinseoul.seoulpublicservice"
         minSdk = 28
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
