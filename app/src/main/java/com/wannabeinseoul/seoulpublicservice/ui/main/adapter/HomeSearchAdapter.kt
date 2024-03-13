825d887d7cac62b93e467434f62d3ae51a24e71d
phs8505 <phs8505@gmail.com>
2024-03-13 17:12:03 +0900

    Layout : 전체적으로 레이아웃 보완
    카테코리 페이지 : 뭔가 많이 됨
    상세 페이지 : 마진 추가와 글자 크기 조절
    홈 페이지 : 이후 알림용 ImageView 추가, 검색 결과 페이지 데이터 조절
    후기 페이지 : 필요없는 코드 제거

--- app/src/main/java/com/wannabeinseoul/seoulpublicservice/ui/main/adapter/HomeSearchAdapter.kt
+++ app/src/main/java/com/wannabeinseoul/seoulpublicservice/ui/main/adapter/HomeSearchAdapter.kt
@@ -1,11 +1,14 @@
 package com.wannabeinseoul.seoulpublicservice.ui.main.adapter
 
+import android.graphics.Color
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import com.wannabeinseoul.seoulpublicservice.R
 import com.wannabeinseoul.seoulpublicservice.databases.ReservationEntity
 import com.wannabeinseoul.seoulpublicservice.databinding.CategoryItemBinding
 
@@ -13,7 +16,8 @@ class HomeSearchAdapter(val items: List<ReservationEntity>) : RecyclerView.Adapt
 
     inner class ViewHolder(private val binding: CategoryItemBinding) : RecyclerView.ViewHolder(binding.root) {
         val ivImage: ImageView = binding.ivCtImage
-        val tvPlaceName: TextView = binding.tvCtPlaceName
+        val tvServiceName: TextView = binding.tvCtServiceName
+        val tvPlaceName: TextView = binding.tvCategoryItemPlace
         val tvIsFree: TextView = binding.tvCtNotFree
         val tvReservationStatus: TextView = binding.tvCtReservationEnd
     }
@@ -27,9 +31,30 @@ class HomeSearchAdapter(val items: List<ReservationEntity>) : RecyclerView.Adapt
         val item = items[position]
         // ivImage에 coil 라이브러리를 사용하여 이미지 로드
         holder.ivImage.load(item.IMGURL)
+        holder.tvServiceName.text = Html.fromHtml(item.SVCNM, Html.FROM_HTML_MODE_LEGACY)
         holder.tvPlaceName.text = item.PLACENM
-        holder.tvIsFree.text = item.PAYATNM
+        holder.tvIsFree.text = item.PAYATNM.take(2)
         holder.tvReservationStatus.text = item.SVCSTATNM
+
+        if (item.PAYATNM.take(2) == "유료") {
+            holder.tvIsFree.setTextColor(Color.parseColor("#5E5E5E"))
+            holder.tvIsFree.setBackgroundResource(R.drawable.background_white_with_rounded_stroke)
+        } else {
+            holder.tvIsFree.setTextColor(Color.parseColor("#FFFFFF"))
+            holder.tvIsFree.setBackgroundResource(R.drawable.background_pointcolor_with_rounded)
+        }
+
+        when (item.SVCSTATNM) {
+            "접수중", "안내중" -> {
+                holder.tvReservationStatus.setTextColor(Color.parseColor("#F8496C"))
+                holder.tvReservationStatus.setBackgroundResource(R.drawable.background_white_with_f8496c_stroke)
+            }
+
+            else -> {
+                holder.tvReservationStatus.setTextColor(Color.parseColor("#5E5E5E"))
+                holder.tvReservationStatus.setBackgroundResource(R.drawable.background_white_with_rounded_stroke)
+            }
+        }
     }
 
     override fun getItemCount() = items.size
