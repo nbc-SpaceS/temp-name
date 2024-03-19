9ea9d18b16e0099ee9dcc8edd07df9d98f3c9b65
jericho31 <a@a.a>
2024-03-19 22:36:24 +0900

    fix: ImageView.loadWithHolder 로딩이미지 FIT_CENTER 적용

--- app/src/main/java/com/wannabeinseoul/seoulpublicservice/util/loadWithHolder.kt
+++ app/src/main/java/com/wannabeinseoul/seoulpublicservice/util/loadWithHolder.kt
@@ -5,13 +5,37 @@ import androidx.annotation.DrawableRes
 import coil.load
 import com.wannabeinseoul.seoulpublicservice.R
 
+//fun ImageView.loadWithHolder(
+//    data: Any?,
+//    @DrawableRes placeholderResId: Int = R.drawable.ic_loading,
+//    @DrawableRes errorResId: Int = R.drawable.place_holder_1,
+//) =
+//    this.load(data) {
+//        crossfade(true)
+//        placeholder(placeholderResId)
+//        error(errorResId)
+//    }
+
 fun ImageView.loadWithHolder(
     data: Any?,
     @DrawableRes placeholderResId: Int = R.drawable.ic_loading,
     @DrawableRes errorResId: Int = R.drawable.place_holder_1,
 ) =
     this.load(data) {
+        val scaleType = this@loadWithHolder.scaleType
         crossfade(true)
-        placeholder(placeholderResId)
-        error(errorResId)
+        target(
+            onStart = {
+                this@loadWithHolder.scaleType = ImageView.ScaleType.FIT_CENTER
+                this@loadWithHolder.setImageResource(placeholderResId)
+            },
+            onError = {
+                this@loadWithHolder.scaleType = scaleType
+                this@loadWithHolder.setImageResource(errorResId)
+            },
+            onSuccess = {
+                this@loadWithHolder.scaleType = scaleType
+                this@loadWithHolder.setImageDrawable(it)
+            }
+        )
     }
