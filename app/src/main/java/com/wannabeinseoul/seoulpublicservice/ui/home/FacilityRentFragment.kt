0acc8ffe2c6cd33272a4aedf16858c9eaa78bdcf
phs8505 <phs8505@gmail.com>
2024-03-21 14:39:48 +0900

    Feat : 대분류마다 카테고리 버튼의 색 변경
    디자이너 튜터님이 주신 링크에서 현재 사용하고 있는 색과 비슷한 계열을 가져와서 적용시킴
    각각의 배경 리소스들을 만듬

--- app/src/main/java/com/wannabeinseoul/seoulpublicservice/ui/home/FacilityRentFragment.kt
+++ app/src/main/java/com/wannabeinseoul/seoulpublicservice/ui/home/FacilityRentFragment.kt
@@ -23,7 +23,7 @@ class FacilityRentFragment : Fragment() {
     private val regionPrefRepository by lazy { (requireActivity().application as SeoulPublicServiceApplication).container.regionPrefRepository }
     private val dbMemoryRepository by lazy { (requireActivity().application as SeoulPublicServiceApplication).container.dbMemoryRepository }
     private val mainViewModel: MainViewModel by activityViewModels()
-    private val adapter by lazy { ItemAdapter(regionPrefRepository) }
+    private val adapter by lazy { ItemAdapter(regionPrefRepository, "시설대관") }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
