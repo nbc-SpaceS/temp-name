16d682311c31f9c95728970048cb5329d40debf7
hyunparrot <coyh1071@gmail.com>
2024-03-27 15:35:20 +0900

    주석, 공백 정리

--- app/src/main/java/com/wannabeinseoul/seoulpublicservice/ui/recommendation/RecommendationViewModel.kt
+++ app/src/main/java/com/wannabeinseoul/seoulpublicservice/ui/recommendation/RecommendationViewModel.kt
@@ -41,17 +41,15 @@ class RecommendationViewModel(
         isLoading.value = true // 로딩 상태로 초기화
 
         viewModelScope.launch(Dispatchers.IO) {
-            val selectedRegions = regionPrefRepository.load() // 선택한 모든 지역을 가져옴
+            val selectedRegions = regionPrefRepository.load()
             val items = mutableListOf<Pair<String, String>>()
             val regionItems = mutableListOf<Pair<String, String>>()
 
-            // 첫 번째 초기화 로직: 지역 외 로직
             items.add(Pair("교육", "교육과 관련된 서비스"))
             items.add(Pair("청소년", "청소년을 위한 서비스"))
             items.add(Pair("장애인", "장애인을 위한 서비스"))
             items.add(Pair("풋살", "풋살에 관한 서비스"))
 
-            // 두 번째 초기화 로직: 다중 선택한 지역에 대한 로직
             selectedRegions.forEach { region ->
                 val regionInfo = region + "에 관한 서비스"
                 regionItems.add(Pair(region, regionInfo))
@@ -145,14 +143,13 @@ class RecommendationViewModel(
 
     suspend fun fetchRegionList() {
         if (!isFirst) {
-            isLoading.postValue(true) // 로딩 상태로 초기화
+            isLoading.postValue(true)
 
             viewModelScope.launch(Dispatchers.IO) {
-                val selectedRegions = regionPrefRepository.load() // 선택한 모든 지역을 가져옴
+                val selectedRegions = regionPrefRepository.load()
 
                 val regionItems = mutableListOf<Pair<String, String>>()
 
-                // 두 번째 초기화 로직: 다중 선택한 지역에 대한 로직
                 selectedRegions.forEach { region ->
                     val regionInfo = region + "에 관한 서비스"
                     regionItems.add(Pair(region, regionInfo))
