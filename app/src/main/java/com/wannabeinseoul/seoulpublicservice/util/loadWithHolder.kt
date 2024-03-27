7b4c400978dba84dbd43319d3ff4a9a985306248
jericho31 <a@a.a>
2024-03-27 20:56:39 +0900

    feat: loadWithHolder 로딩 실패 로그

--- app/src/main/java/com/wannabeinseoul/seoulpublicservice/util/loadWithHolder.kt
+++ app/src/main/java/com/wannabeinseoul/seoulpublicservice/util/loadWithHolder.kt
@@ -1,8 +1,12 @@
 package com.wannabeinseoul.seoulpublicservice.util
 
+import android.util.Log
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
 import coil.load
+import coil.request.ErrorResult
+import coil.request.ImageRequest
+import com.wannabeinseoul.seoulpublicservice.BuildConfig
 import com.wannabeinseoul.seoulpublicservice.R
 
 //fun ImageView.loadWithHolder(
@@ -22,8 +26,14 @@ fun ImageView.loadWithHolder(
     @DrawableRes errorResId: Int = R.drawable.place_holder_1,
 ) =
     this.load(data) {
+        val JJTAG = "jj-ImageView.loadWithHolder"
         val scaleType = this@loadWithHolder.scaleType
         crossfade(true)
+        if (BuildConfig.DEBUG) {
+            listener(onError = { request: ImageRequest, result: ErrorResult ->
+                Log.w(JJTAG, "$this request.data: ${request.data}", result.throwable)
+            })
+        }
         target(
             onStart = {
                 this@loadWithHolder.scaleType = ImageView.ScaleType.FIT_CENTER
